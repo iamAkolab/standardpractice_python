482ea15a82e8be48fea691de1b440b01ef73121e
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-05 19:59:07 -0600

    Update and rename docstrings_complete.md to docstrings_complete.py

--- writing_functions_in_python/week1_Best_practices/docstrings_complete.py
+++ writing_functions_in_python/week1_Best_practices/docstrings_complete.py
@@ -1,9 +1,10 @@
 # Crafting a docstring
-
+'''
 You've decided to write the world's greatest open-source natural language processing Python package
 The first function you write is count_letter(). It takes a string and a single letter and returns the number of times the letter appears in the string. 
 You want the users of your open-source package to be able to understand how this function works easily, so you will need to give it a docstring. 
 Build up a Google Style docstring for this function by following these steps.
+'''
 
 
 def count_letter(content, letter):
@@ -25,18 +26,21 @@ def count_letter(content, letter):
   return len([char for char in content if char == letter])
   
 # Retrieving docstrings
- 
+
+'''
 You and a group of friends are working on building an amazing new Python IDE (integrated development environment -- like PyCharm, Spyder, Eclipse, Visual Studio, etc.). The team 
 wants to add a feature that displays a tooltip with a function's docstring whenever the user starts typing the function name. That way, the user doesn't have to go elsewhere to 
 look up the documentation for the function they are trying to use. You've been asked to complete the build_tooltip() function that retrieves a docstring from an arbitrary function.
+'''
 
-You will be reusing the count_letter() function that you developed in the last exercise to show that we can properly extract its docstring
-Begin by getting the docstring for the function
+# You will be reusing the count_letter() function that you developed in the last exercise to show that we can properly extract its docstring
+# Begin by getting the docstring for the function
 # Get the "count_letter" docstring by using an attribute of the function
 docstring = count_letter.__doc__
 
 border = '#' * 28
 print('{}\n{}\n{}'.format(border, docstring, border))
+
 ############################
 Count the number of times `letter` appears in `content`.
 
