8fa36853e6ad1c8c522a2b6021fcd82b68a18b3d
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-11 00:08:37 -0600

    Update zen_of_python.py

--- Writing_Efficient_Python_Code/week_1_foundations_for_efficiencies/zen_of_python.py
+++ Writing_Efficient_Python_Code/week_1_foundations_for_efficiencies/zen_of_python.py
@@ -1,1 +1,31 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## The Zen of Python by Tim Peters
 
+# The Zen of Python is a collection of 19 "guiding principles" for writing computer programs that influence the design of the Python programming language.
+#  Software engineer Tim Peters wrote this set of principles and posted it on the Python mailing list in 1999.[2] Peters's list left open a 20th principle "for Guido to fill in"
+# It is also included as an Easter egg in the Python interpreter, where it can be displayed by entering.
+
+>>> import this
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## Efficient code are Code that executes quickly for the task at hand, minimizes the memory footprint and follows Python's coding style principles.
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
