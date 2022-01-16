961e9e353e303592b25288c22e19f917090a0565
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-15 22:46:02 -0600

    Create numpyarrays.py

---
+++ Writing_Efficient_Python_Code/week_1_foundations_for_efficiencies/numpyarrays.py
@@ -0,0 +1,8 @@
+## NUMPY ARRAYS
+
+# Numpy is the fundamental package for scientific computing. Numpy arrays are homogeneous, they must contain element of the same type
+
+import numpy as np
+
+nums_np  = np.array(range(5))
+# array([1,2,3,4])
