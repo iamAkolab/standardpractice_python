5fb35c1cfd54a07847dc7cc2278153334f324cf6
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-21 22:51:55 -0600

    Update examining_runtime.py

--- Writing_Efficient_Python_Code/week_2_timing_and_profiling_code/examining_runtime.py
+++ Writing_Efficient_Python_Code/week_2_timing_and_profiling_code/examining_runtime.py
@@ -12,8 +12,25 @@
 # To calculate runtime with IPython magic command %timeit
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Using %timeit
+## Using %timeit
 
+# code to be timed
+import numpy as np
+
+rand_nums = np.random.num(1000)
+
+# timing with %timeit
+%timeit rand_nums = np.random.num(1000)
+# 8.61 us +/- 69.1 ns per loop (mean +/- std. dev. 7 runs, 100000 loops each)
+
+
+# specifying number of runs/loops
+# setting the number of runs (-r)  and / or loops (-n)
+
+# Set number of runs to 2 (-r)
+# Set number of loops to 10
+%timeit -r2 -n10 rand_nums = np.random.num(1000)
+# 16.9 us +/- 5.14 ns per loop (mean +/- std. dev. 2 runs, 10 loops each)
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
