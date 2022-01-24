1e5f0bbd9896d93660e776f041672c99370a43d3
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-24 00:20:03 -0600

    Update using_context_managers.py

--- writing_functions_in_python/week2_Context_managers/using_context_managers.py
+++ writing_functions_in_python/week2_Context_managers/using_context_managers.py
@@ -45,3 +45,38 @@ for word in text.split():
 print('Lewis Carroll uses the word "cat" {} times'.format(n))
 # Lewis Carroll uses the word "cat" 24 times
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# The speed of cats
+
+# You're working on a new web service that processes Instagram feeds to identify which pictures contain cats (don't ask why -- it's the internet). 
+# The code that processes the data is slower than you would like it to be, so you are working on tuning it up to run faster. Given an image, image, 
+# you have two functions that can process it:
+
+    # process_with_numpy(image)
+    # process_with_pytorch(image)
+
+# Your colleague wrote a context manager, timer(), that will print out how long the code inside the context block takes to run. 
+# She is suggesting you use it to see which of the two options is faster. Time each function to determine which one to use in your web service.
+
+
+# Use the timer() context manager to time how long process_with_numpy(image) takes to run.
+# Use the timer() context manager to time how long process_with_pytorch(image) takes to run.
+
+image = get_image_from_instagram()
+
+# Time how long process_with_numpy(image) takes to run
+with timer():
+  print('Numpy version')
+  process_with_numpy(image)
+
+# Time how long process_with_pytorch(image) takes to run
+with timer():
+  print('Pytorch version')
+  process_with_pytorch(image)
+  
+# Numpy version
+# Processing..........done!
+# Elapsed: 1.53 seconds
+# Pytorch version
+# Processing..........done!
+# Elapsed: 0.33 seconds
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
