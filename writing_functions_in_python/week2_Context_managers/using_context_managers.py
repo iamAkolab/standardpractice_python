e6cad95ef502cdd0c5a45a702c27d50c89a06438
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-01-23 23:53:30 -0600

    Update using_context_managers.py

--- writing_functions_in_python/week2_Context_managers/using_context_managers.py
+++ writing_functions_in_python/week2_Context_managers/using_context_managers.py
@@ -1,2 +1,18 @@
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-##
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## What is a context manager
+
+# A context manager is a type of function that sets up a context for your code to run in, runs your code, and then removes the context.
+
+# A real world example
+
+with open('my_file.txt') as my_file:
+  text = my_file.read()
+  length = len(text)
+  
+print('The file is {} characters long'.format(length))
+
+# open() does three things:
+  # sets up a context by opening a file
+  # lets you run any code you want on that file
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
